f780e73b3648dff7f66479f814991bda3c6ca03f
Dominik Brożyna <79914884+dbrozyna15@users.noreply.github.com>
2021-11-22 10:02:45 +0100

    Class  6

---
+++ ComputerFile.java
@@ -0,0 +1,14 @@
+public class ComputerFile
+{
+    int id;
+    String name;
+    String type;
+    String extension;
+    String location;
+    int size;
+    
+    public void delete() {}
+    public void createShortcut() {}
+    public void share(int userID) {}
+    public void rename(String newName) {}
+}
